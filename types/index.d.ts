80107fd5607dbfda9aed9c069c61b00329838134
Dima Vyshniakov <1158959+morewings@users.noreply.github.com>
2020-11-11 13:37:53 +0100

    Add types (#13)
    * add types closes #10
    
    * 0.2.1
    
    Co-authored-by: Dima Vyshniakov <dmytro.vishyakov@outfittery.de>

---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+declare module 'css-vars-hook' {
+  import * as React from 'react';
+
+  type ThemeObject = Record<string, string>;
+
+  type Value = string | number;
+
+  interface HookInterface {
+    ref: React.RefObject<HTMLInputElement>;
+    setRef: (element: HTMLElement) => void;
+    style: ThemeObject;
+    setVariable: (variableName: string, value: Value) => void;
+    getVariable: (variableName: string) => string;
+    removeVariable: (variableName: string) => void;
+  }
+
+  export function useTheme(theme: ThemeObject): HookInterface;
+
+  export function useVariable(name: string, value: Value): HookInterface;
+}
