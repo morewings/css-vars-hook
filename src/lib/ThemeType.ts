4a0a9ccf3637711d6a5a953dbc7102ae39689f40
Dima Vyshniakov <1158959+morewings@users.noreply.github.com>
2023-03-09 16:50:05 +0100

    Add API docs (#127)

--- src/lib/ThemeType.ts
+++ src/lib/ThemeType.ts
@@ -1,1 +1,8 @@
+/**
+ * @public
+ * @name
+ * ThemeType
+ * @description
+ * Base theme type to extend from.
+ */
 export type ThemeType = Record<string, string>;
--- src/lib/useVariable.ts
+++ src/lib/useVariable.ts
@@ -1,5 +1,11 @@
 import {useTheme} from './useTheme';
 
+/**
+ * @function
+ * @public
+ * @name useVariable
+ * @description React hook to apply single CSS variable and manipulate it.
+ */
 export const useVariable = (name: string, value: string) => {
   const {ref, setRef, style, setVariable, getVariable, removeVariable} = useTheme({[name]: value});
   return {
