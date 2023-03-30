2d18adac5b582dec235163242b0a6e4144768c5b
Dima Vyshniakov <morewings@gmail.com>
2023-03-30 23:28:26 +0200

    remove color and movement demos

--- src/environment/App/App.tsx
+++ src/environment/App/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-import {DemoMovement} from '../DemoMovement';
 import {DemoRoot} from '../DemoRoot';
 import {RootThemeProvider} from '../../lib';
-import {DemoColor} from '../DemoColor';
 import {DemoLocal} from '../DemoLocal';
 import {Style} from '../Examples/Style';
 import {Mouse} from '../Examples/Mouse';
@@ -16,12 +14,10 @@ export const App = () => (
             borderColor: 'violet',
         }}>
         <div className={classes.container}>
-            <DemoColor />
-            {/*<DemoMovement />*/}
-            <DemoRoot />
-            <DemoLocal />
             <Style />
             <Mouse />
+            <DemoRoot />
+            <DemoLocal />
         </div>
     </RootThemeProvider>
 );
--- src/environment/DemoColor/index.ts
+++
@@ -1,1 +0,0 @@
-export {DemoColor} from './DemoColor';
