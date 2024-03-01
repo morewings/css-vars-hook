650594db0d3509205b95585f2770aedf702e1a96
Dima Vyshniakov <morewings@gmail.com>
2024-03-01 16:30:17 +0100

    ci(release): add alpha and beta pre-release branches config

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-    branches: ['master'],
+    branches: ['master', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
