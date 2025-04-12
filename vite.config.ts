6428fe89e06e720e0c1fac6929af833aeee7d2dd
Dima Vyshniakov <morewings@gmail.com>
2025-04-12 21:54:34 +0200

    feat(theme): refactor theme setting logic

--- vite.config.ts
+++ vite.config.ts
@@ -7,37 +7,31 @@ import external from '@yelo/rollup-node-external';
 import dts from 'vite-plugin-dts';
 import postcssPresetEnv from 'postcss-preset-env';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
         alias: hq.get('rollup'),
     },
-    plugins: [react(), dts({rollupTypes: true, exclude: ['**/*.stories.(ts|tsx)']})],
+    plugins: [react(), dts({rollupTypes: true})],
     build: {
         sourcemap: true,
         lib: {
-            // Could also be a dictionary or array of multiple entry points
             entry: resolve(__dirname, 'src/lib/index.ts'),
             name: 'css-vars-hook',
-            // the proper extensions will be added
             fileName: 'index',
         },
         rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
             external: external(),
             output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
                 globals: {
                     react: 'React',
                 },
+                banner: "'use client';",
             },
         },
     },
     css: {
         postcss: {
-            plugins: [postcssPresetEnv({})],
+            plugins: [postcssPresetEnv({stage: 1})],
         },
     },
 });
