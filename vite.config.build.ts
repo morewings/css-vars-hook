d1b08c25fb7722ad663e027c0049329d9e86a0d9
Dima Vyshniakov <morewings@gmail.com>
2025-09-28 22:49:26 +0200

    refactor(alias): switch to alias-kitchen from alias-hq

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -1,13 +1,12 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-// @ts-expect-error no types
-import hq from 'alias-hq';
+import {kitchen} from 'alias-kitchen';
 import postcssPresetEnv from 'postcss-preset-env';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     resolve: {
-        alias: hq.get('rollup'),
+        alias: kitchen({recipe: 'vite'}),
     },
     plugins: [react()],
     base: '/css-vars-hook/',
